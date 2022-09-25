e570d95d1895cf37a07802f1343ef249df4e869d
James Scott <jsa.codes@gmail.com>
2022-09-25 10:20:57 -0500

    Created a new button called Show All. This button will be seen by employees only. Using a ternary statement, If the logged in user is an employee, then show this button. This button also has a clickEvent, like the Emergecy Only button, which also has a clickEvent on it that listens for the click and sets the state of emergency to true, on it that sets the state of emergency back to false, which then shows all of the tickets. If the logged in user is a customer then neither of these buttons show up.

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -7,7 +7,12 @@ export const TicketList = () => {
     const [tickets, setTickets] = useState([]);
     // State #2) - Showing filtered tickets based on conditions of "employee or customer"
     const [filteredTickets, setFilteredTickets] = useState([]);
+    // State #3) - Showing filtered tickets based on whether or not the employee clicked the "Emergency Only" button.
+    const [emergency, setEmergency] = useState(false)
 
+    // honeyUserObject has:
+    //  - primary key of the user
+    //  - staff key on it (can be true or false)
     const localHoneyUser = localStorage.getItem("honey_user")
     const honeyUserObject = JSON.parse(localHoneyUser)
 
@@ -37,30 +42,66 @@ export const TicketList = () => {
                 setFilteredTickets(tickets)
             }
             else {
-                // For customers — filter the tickets, check each ticket to see if it's userId is equal to the logged in user's id.
+                // For customers — filter the tickets, check each ticket to see if its userId is equal to the logged in user's id.
                 const myTickets = tickets.filter(ticket => ticket.userId === honeyUserObject.id)
                 setFilteredTickets(myTickets)
             }
         }, [tickets] // Observing all tickets. IF anything changes, i.e. - a different user logs in, then filter those tickets by matching the userId of those tickets to the logged in user's id.
     )
 
+    // State Change #3) - Filtered Tickets will now show ONLY Emergency Tickets
+
+    useEffect(
+        () => {
+            if (emergency) {
+                const emergencyTickets = tickets.filter(ticket => ticket.emergency === true)
+                setFilteredTickets(emergencyTickets)
+            }
+            else {
+                setFilteredTickets(tickets)
+            }
+        }, [emergency]
+    )
+
+    // The button is a "user interaction". They want to change the "state" of the component when they click on this Emergency Button
+
+    // IF honeyUserObject is staff (which is true)
+    //      - Show the "Emergency Only" button
+    // Otherwise, show an empty "" (which will show nothing — no button will appear)
+    // This way, Employees can click the "Emergency Only" button to see those tickets that are "emergency tickets (urgent tickets)", but customers will only see their tickets they've submitted displayed
     return (
         <>
+            {
+                honeyUserObject.staff ? (
+                    <>
+                        <button
+                            onClick={
+                                () => {
+                                setEmergency(true);
+                            }}
+                        >Emergency Only</button>
+                        <button
+                            onClick={
+                                () => {
+                                    setEmergency(false)
+                                }
+                            }>Show All</button>
+                    </>
+                ) : (
+                    ''
+                )
+            }
             <h2>List of Tickets</h2>
 
             <article className='tickets'>
-                {
-                    filteredTickets.map(
-                        (ticket) => {
-                            return <section className='ticket'>
-                                <header>{ticket.description}</header>
-                                <footer>Emergency: {ticket.emergency ? "🆘" : "No"}</footer>
-
-                            </section>
-                        }
-                    )
-                }
-
+                {filteredTickets.map((ticket) => {
+                    return (
+                        <section className='ticket'>
+                            <header>{ticket.description}</header>
+                            <footer>Emergency: {ticket.emergency ? '🆘' : 'No'}</footer>
+                        </section>
+                    );
+                })}
             </article>
         </>
     );
