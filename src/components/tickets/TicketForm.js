083bc5a58662390745bd4e94ec0c7e4a4d04883f
James Scott <jsa.codes@gmail.com>
2022-09-25 11:39:39 -0500

    Added an onClick to the Submit Ticket button in the TicketForm component. It invokes the handleSaveButtonClick when the Submit Ticket button is clicked. It is expecting the clickEvent as its argument, so that is passed in to the onClick and the handleSaveButtonClick function. Also added a consolelog to test the button when clicked.

--- src/components/tickets/TicketForm.js
+++ src/components/tickets/TicketForm.js
@@ -1,3 +1,4 @@
+import { click } from '@testing-library/user-event/dist/click'
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
@@ -20,6 +21,7 @@ export const TicketForm = () => {
 
     const handleSaveButtonClick = (event) => {
         event.preventDefault()
+        console.log("You clicked the button!");
 
         // TODO: Create the object to be saved to the API
 
@@ -28,43 +30,45 @@ export const TicketForm = () => {
     }
 
     return (
-        <form className="ticketForm">
-            <h2 className="ticketForm__title">New Service Ticket</h2>
+        <form className='ticketForm'>
+            <h2 className='ticketForm__title'>New Service Ticket</h2>
             <fieldset>
-                <div className="form-group">
-                    <label htmlFor="description">Description:</label>
+                <div className='form-group'>
+                    <label htmlFor='description'>Description:</label>
                     <input
-                        required autoFocus
-                        type="text"
-                        className="form-control"
-                        placeholder="Brief description of problem"
+                        required
+                        autoFocus
+                        type='text'
+                        className='form-control'
+                        placeholder='Brief description of problem'
                         value={ticket.description}
-                        onChange={
-                            (evt) => {
-                                const copy = {...ticket}
-                                copy.description = evt.target.value
-                                update(copy)
-                            }
-                        } />
+                        onChange={(evt) => {
+                            const copy = { ...ticket };
+                            copy.description = evt.target.value;
+                            update(copy);
+                        }}
+                    />
                 </div>
             </fieldset>
             <fieldset>
-                <div className="form-group">
-                    <label htmlFor="name">Emergency:</label>
-                    <input type="checkbox"
+                <div className='form-group'>
+                    <label htmlFor='name'>Emergency:</label>
+                    <input
+                        type='checkbox'
                         value={ticket.emergency}
-                        onChange={
-                            (evt) => {
-                                const copy = {...ticket}
-                                copy.emergency = evt.target.checked
-                                update(copy)
-                            }
-                        } />
+                        onChange={(evt) => {
+                            const copy = { ...ticket };
+                            copy.emergency = evt.target.checked;
+                            update(copy);
+                        }}
+                    />
                 </div>
             </fieldset>
-            <button className="btn btn-primary">
+            <button 
+                onClick={(clickEvent) => handleSaveButtonClick(clickEvent)} 
+                className='btn btn-primary'>
                 Submit Ticket
             </button>
         </form>
-    )
+    );
 }
