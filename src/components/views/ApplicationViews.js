7c8139f8144e33324ef02616846719e829ad47f6
James Scott <jsa.codes@gmail.com>
2022-09-26 10:31:57 -0500

    Added additional comments describing what ApplicationViews does

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -5,6 +5,8 @@ import { EmployeeViews } from './EmployeeViews';
 // IS logged in user a Staff member or not.
 //      - IF so, then show the correct views.
 //          - REMEMBER: Staff is a property of the honeyUserObject
+
+// In ApplicationViews we can customize the routes we're gonna support depending on the user type
 export const ApplicationViews = () => {
 
     const localHoneyUser = localStorage.getItem('honey_user');
