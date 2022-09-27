6cc7d54ca6c418e4202ba1121804a8fe85dd4281
James Scott <jsa.codes@gmail.com>
2022-09-27 11:01:34 -0500

    In EmployeeDetails, created new state for employee. Then created a useEffect that fetches from json, filters down the request to return the userId that matches the employeeId. Did this using query string parameters. Captured that data that we get back from json in the data variable, created a new variable called singleEmployee, because the returned array will only ever have one object in the array, since we are looking for the particular user. Did this by assigning data[0] to singleEmployee. Then updated the state of the employeeId we are observing, in the dependency array, by passing in the returned value stored in singleEmployee

--- src/components/employees/Employee.js
+++ src/components/employees/Employee.js
@@ -3,12 +3,14 @@
 
 import { Link } from 'react-router-dom';
 
+
+
 // EmployeeList has access to state, but Employee does not. Which is why these props are being passed into the Employee component.
 export const Employee = ({id, fullName, email}) => {
     return (
         <section className='employee'>
             <div>
-                <Link to={`/employees/{id}`}>Name: {fullName}</Link>
+                <Link to={`/employees/${id}`}>Name: {fullName}</Link>
             </div>
             <div>Email: {email}</div>
         </section>
--- src/components/employees/EmployeeDetails.js
+++ src/components/employees/EmployeeDetails.js
@@ -1,9 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 export const EmployeeDetails = () => {
     const {employeeId} = useParams()
     const [employee, updateEmployee] = useState()
+
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/employees?_expand=user&_embed=employeeTickets&userId=${employeeId}`)
+            .then(response => response.json())
+            // Capture the data in the response that we get back from json
+            .then((data) =>  {
+                const singleEmployee = data[0]
+                updateEmployee(singleEmployee)
+            })
+        }, [employeeId]
+    )
     
     return <></>
 }
